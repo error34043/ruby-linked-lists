9f3447fe4158c010ac3f0718dfe30be32ffd8b16
Sanjana <sanjhemdev@gmail.com>
2020-08-28 05:53:16 +0530

    Create skeleton structure

--- main.rb
+++ main.rb
@@ -1,2 +1,57 @@
 # frozen-string-literal: true
 
+# Represents the full list
+class LinkedList
+  def append(value)
+
+  end
+
+  def prepend(value)
+
+  end
+
+  def size
+
+  end
+
+  def head
+
+  end
+
+  def tail
+
+  end
+
+  def at(index)
+
+  end
+
+  def pop
+
+  end
+
+  def contains?(value)
+
+  end
+
+  def find(value)
+
+  end
+
+  def to_s
+
+  end
+
+  def insert_at(value, index)   # Extra credit
+
+  end
+
+  def remove_at(index)          # Extra credit
+
+  end
+end
+
+# Represents a single node
+class Node
+
+end
